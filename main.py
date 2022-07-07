eb84ac38dc2212b61febf237beafb8a002af37c3
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-07-07 08:39:02 +0530

    c0.1.0

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ import scipy
 # Alanine scan.
 #----------------------------------------
 # [TODO]
-# 
 
 #========================================
 # Groups
-AA_GROUPS = {
-	'aliphatic' : ["G", "A", "V", "L", "I", "P"],
-	'polar neutral' : ["S", "T"],
-	'amide' : ["N", "Q"],
-	'sulfur-containing' : ["C", "M"],
-	'aromatic' : ["F", "Y", "W"],
-	'anionic' : ["D", "E"],
-	'cationic' : ["H", "K", "R"],
-	'hydrophobic' : ["V", "I", "L", "F", "W", "Y", "M"],
-	'hydrophilic' : ["S", "T", "H", "N", "Q", "E", "D", "K", "R"],
-	'positively-charged' : ["K", "R", "H"],
-	'negatively-charged' : ["D", "E"]
-}
+def groups(sequence, replacement):
+	AA_GROUPS = {
+		'polar_uncharged': ['S', 'T', 'C', 'P', 'N', 'Q'],
+		'positively_charged': ['K', 'R', 'H'],
+		'negatively_charged': ['D', 'E'],
+		'nonpolar_aliphatic': ['G', 'A', 'V', 'L', 'M', 'J', 'I'],
+		'nonpolar_aromatic': ['F', 'Y', 'W']
+	}
+
+	# Decode the replacement format
+
+	for types in AA_GROUPS.keys():
+		for AA in AA_GROUPS[types]:
+			print(AA)
+	return True
 
 #========================================
 # Dipeptide filter.
@@ -38,17 +39,18 @@ def dipeptide_matches(sequence):
 	return matches
 
 #========================================
+# SKIPPED, FOR NOW.
 # Intein sequences.
 #----------------------------------------
-def intein_matches(sequence):
-	inteins = ["CRAZY_SEQUENCE", "ANOTHER_SEQUENCE"] # Put the source as a dictionary or an array, preferably.
+# def intein_matches(sequence):
+# 	inteins = ["CRAZY_SEQUENCE", "ANOTHER_SEQUENCE"] # Put the source as a dictionary or an array, preferably.
 
-	for intein in inteins:
-		match = str(sequence).find(intein)
-		if match:
-			return False
-		else:
-			return True
+# 	for intein in inteins:
+# 		match = str(sequence).find(intein)
+# 		if match:
+# 			return False
+# 		else:
+# 			return True
 
 #========================================
 # Cleavage sites.
@@ -64,6 +66,7 @@ def intein_matches(sequence):
 def __main__():
 	INPUT = "HHHDAASDLKJASD" # Peptide sequence here.
 
+	print("Groups filter: " + str(groups(INPUT)))
 	print("Dipeptides?: " + str(dipeptide_matches(INPUT)))
 	print("Inteins?: " + str(intein_matches(INPUT)))
 
