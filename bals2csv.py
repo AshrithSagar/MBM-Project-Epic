8ef3dc28d95f2df9ca2da5e92350d93de6255ef1
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-07-09 00:41:18 +0530

    c0.5.1

--- bals2csv.py
+++ bals2csv.py
@@ -3,12 +3,12 @@
 """
 import sys
 import json
-import csv
+import pandas as pd
 
-with open(sys.argv[1], 'r') as file:
+with open(sys.argv[1], "r") as file:
     contents = file.readlines()
 
-json = {
+json_file = {
 	"Index": [],
 	"Number": [],
 	"Name": [],
@@ -24,57 +24,63 @@ json = {
 
 for line in contents:
 	value = line[0:9].strip()
-	try: json['Index'].append(int(value))
+	try: json_file["Index"].append(int(value))
 	except: pass
 
 	value = line[10:14].strip()
-	try: json['Number'].append(int(value))
+	try: json_file["Number"].append(int(value))
 	except: pass
 
 	value = line[15:20].strip()
-	try: json['Name'].append(str(value))
+	try: json_file["Name"].append(str(value))
 	except: pass
 
 	value = line[21:25].strip()
-	try: json['Chain'].append(str(value))
+	try: json_file["Chain"].append(str(value))
 	except: pass
 
 	value = line[26:39].strip()
-	try: json['InterDG'].append(float(value))
+	try: json_file["InterDG"].append(float(value))
 	except: pass
 
 	value = line[40:51].strip()
-	try: json['InterDDG'].append(float(value))
+	try: json_file["InterDDG"].append(float(value))
 	except: pass
 
 	value = line[52:60].strip()
-	try: json['NormTerDDG'].append(float(value))
+	try: json_file["NormTerDDG"].append(float(value))
 	except: pass
 
 	value = line[62:72].strip()
-	try: json['IntraDG'].append(float(value))
+	try: json_file["IntraDG"].append(float(value))
 	except: pass
 
 	value = line[73:85].strip()
-	try: json['IntraDDG'].append(float(value))
+	try: json_file["IntraDDG"].append(float(value))
 	except: pass
 
 	value = line[86:101].strip()
-	try: json['NormTraDDG'].append(float(value))
+	try: json_file["NormTraDDG"].append(float(value))
 	except: pass
 
 	value = line[102:107].strip()
-	try: json['ChainAtoms'].append(int(value))
+	try: json_file["ChainAtoms"].append(int(value))
 	except: pass
 
-print(json)
+json_file = json.dumps(json_file)
 
+# Save as .json
 file = sys.argv[1]
-file = file.replace('.bals', '.json')
-file = open(file, 'w')
-file.write(str(json))
+file = file.replace(".bals", ".json")
+file = open(file, "w")
+file.write(str(json_file))
 
+# Save as .csv
 file = sys.argv[1]
-file = file.replace('.bals', '.json')
-file = open(file, 'w')
-file.write(str(json))
+json_file = file.replace(".bals", ".json")
+print(json_file)
+df = pd.read_json(json_file)
+# csv = df.to_csv()
+# file = file.replace(".bals", ".csv")
+# file = open(file, "w")
+# file.write(csv.to_string())
